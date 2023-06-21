c04485b1f71cd91b04d64a9bd64dee71c08ee582
kumareshinnoraft <kumareh.baksi@innoraft.com>
2023-06-21 16:11:32 +0530

    FT2023-305: Commiting all files before merging with FT2023-304.

---
+++ web/modules/custom/routing/routing.api.php
@@ -0,0 +1,18 @@
+<?php 
+
+/**
+ * This function is for documentation purposes only.
+ *
+ * @param  mixed $number1
+ *   This is the first number that is going to be added.
+ * @param  mixed $number2
+ *   This is the second number that is going to be added.
+ * @return mixed
+ *   The return type is mixed and parameter could be anything.
+ */
+function hook_add_numbers($number1, $number2) {
+  // Example code: Add two numbers and return the result.
+  $result = $number1 + $number2;
+  // Return the result.
+  return $result;
+}
